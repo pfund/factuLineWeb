19436e3a489b970005b16751895e88052f353557
Thomas Pfund <thomas.pfund@gmail.com>
2014-03-19 22:34:24 +0100

    Delete working and showing only by correct date

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,22 +6,53 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
-  .controller('MyCtrl2', ['$scope', '$routeParams', 'FactuLineRest', 
-		  function($scope, $routeParams, FactuLineRest) {
+  .controller('MyCtrl2', ['$scope', '$routeParams', 'FactuLineRest', 'ConsultsFactory',
+		  function($scope, $routeParams, FactuLineRest, ConsultsFactory) {
 
-    $scope.dayId = $routeParams.dayId;
+    $scope.dateConsult = moment($routeParams.dayId, "DD.MM.YYYY").toDate();
+    console.log($scope.dateConsult);
 
     $scope.newInput = new Consult();
-
-    FactuLineRest.query( function(data) {
-      $scope.consults = data;
-    });
+    $scope.newInput.dateConsult = $scope.dateConsult;
+      
+//    FactuLineRest.query( function(data) {
+//      $scope.consults = data;
+//    });
+    ConsultsFactory.getByDateConsult({'dateConsult':$scope.dateConsult}, 
+      function(data) {
+        $scope.consults = data;
+      }
+    );
 
     $scope.insert = function() {
+      var stringBirthDate = $scope.newInput.birthDate;
+      if (stringBirthDate) {
+	// Start by checking the YYYY format first, otherwise moment will take 2030(YYYY) as a 20 (YY)
+	var momentBirthDate = moment(stringBirthDate, "DD.MM.YYYY");
+        if (!momentBirthDate.isValid()) {
+          momentBirthDate = moment(stringBirthDate, "DD.MM.YY");
+        }
+        if (!momentBirthDate.isValid()) {
+          // TODO alert the user and abort sending
+	  console.log("not valid");
+	  return;
+	} else {
+          $scope.newInput.birthDate = momentBirthDate.toDate();
+	}
+      }
       $scope.consults.push($scope.newInput);
       FactuLineRest.save({}, $scope.newInput);
       $scope.newInput = new Consult();
+      $scope.newInput.dateConsult = $scope.dateConsult;
     };
+
+    $scope.delete = function(id) {
+      FactuLineRest.delete({'id':id});
+      FactuLineRest.query( function(data) {
+        $scope.consults = data;
+      });
+    };
+    
   }])
 
 
--- app/js/services.js
+++ app/js/services.js
@@ -6,9 +6,17 @@ var factuLineServices = angular.module('myApp.services', ['ngResource']);
 
 factuLineServices.factory('FactuLineRest', ['$resource',
   function($resource){
-    return $resource('http://localhost:8080/factuLineRest/consult/:consultId', {}, {
-      query: {method:'GET', params:{consultId:''}, isArray:true},
-      post: {method:'POST'}
+    return $resource('http://localhost:8080/factuLineRest/consult/:id', {}, {
+      query: {method:'GET', params:{id:''}, isArray:true},
+      post: {method:'POST'},
+      delete: {method:'DELETE', params:{id: '@id'}}
     });
  }]);
 
+factuLineServices.factory('ConsultsFactory', ['$resource', 
+  function($resource) {
+    return $resource('http://localhost:8080/factuLineRest/consult/getByDateConsult/:dateConsult', {} , {
+      getByDateConsult: {method:'GET', params:{dateConsult: '@dateConsult'}, isArray:true}
+    });
+  }
+]);
