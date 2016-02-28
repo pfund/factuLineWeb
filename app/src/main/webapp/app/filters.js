08a5202d858272f7b0b1cffe81dac97d852b4360
Thomas Pfund <thomas.pfund@gmail.com>
2016-02-28 22:08:52 +0100

    Added the toFixed filter (set the number of decimals to show without thousand separators)

--- app/src/main/webapp/app/filters.js
+++ app/src/main/webapp/app/filters.js
@@ -2,9 +2,15 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
+angular.module('myApp.filters', [])
+    .filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }])
+  .filter('toFixed', function ($filter) {
+  return function (input, places) {
+    if (isNaN(input)) return input;
+    return input.toFixed(places);
+  };
+});
